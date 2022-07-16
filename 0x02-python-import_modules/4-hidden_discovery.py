5a45609959a7f629d94921dba88f2d8f3dd4ac33
Modupe <mistura.junaid024.com>
2022-07-16 13:28:39 -0700

    Py2

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,9 @@
-#!/usr/bin/python3.4
-if __name__ == "__main__":
-    from hidden_4 import *
-    for s in dir():
-        if s[:2] != "__":
-            print("{}".format(s))
+#!/usr/bin/python3
+import hidden_4
+
+if __name__ == '__main__':
+    def_names = dir(hidden_4)
+
+    for i in range(len(def_names)):
+        if def_names[i][:2] != '__':
+            print(def_names[i])
